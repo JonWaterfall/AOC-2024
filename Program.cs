6a0468df17098d1ba6dc1473b2b71dc6a09c9173
JonWaterfall <jon.gunnar.fossum@gmail.com>
2025-08-26 13:16:13 +0200

    Basic csharp console project setup

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class MyProgram
+{
+static int Main(string[] args)
+{
+    String? argstring = args.ToString();
+    if (argstring == null)
+    {
+        Console.WriteLine("whole number 1-25 required as an argument");
+        return 1;
+    }
+    try
+    {
+        int daytorun = Int32.Parse(argstring);
+
+        // run the calendar day code
+    }
+    catch (System.Exception e)
+    {
+        Console.WriteLine("whole number 1-25 required as an argument");
+        Console.WriteLine(e.Message);
+        return 1;
+        throw;
+    }
+
+    return 0;
+}
+}
---
+++ days/day01.cs
@@ -0,0 +1,10 @@
+
+
+static class Day01
+{
+    // result is null if something went wrong
+    public static int? Part1()
+    {
+        return null;
+    }
+}
