39d519a0199dce466e60b838fd1a45d5e9d73681
JonWaterfall <jon.gunnar.fossum@gmail.com>
2025-08-26 15:30:18 +0200

    Add metadata struct and remove nullable property of return type
    Structs can't be nullable without fighting for it, but I don't need that in my usecase.

--- util.cs
+++ util.cs
@@ -2,18 +2,35 @@
 
 static class Util
 {
+    public struct FileData(string s, int i)
+    {
+        public string inputtext = s;
+        public int lineCount = i;
+    }
+
     // expects file to only contain UTF-16 characters
-    // returns null when something went wrong.
-    static string? ReadFileToString(string path)
+    // entire file will be read once to count the number of lines
+    public static FileData ReadFileToString(string path)
     {
         try
         {
-            return File.ReadAllText(path);
+            using StreamReader r = new StreamReader(path);
+
+            string totalText = "";
+            int lineCount = 0;
+            string? line = r.ReadLine();
+            while (line != null)
+            {
+                lineCount++;
+                totalText += line;
+
+                line = r.ReadLine();
+            }
+            return new FileData(totalText, lineCount);
         }
         catch (System.Exception e)
         {
             Console.Write(e.Message);
-            return null;
             throw;
         }
     }
