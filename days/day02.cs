d58c2e4d64c8775cfc4c0b3826670963b0d9abd0
JonWaterfall <jon.gunnar.fossum@gmail.com>
2025-09-01 10:08:55 +0200

    fix spelling error

--- days/day02.cs
+++ days/day02.cs
@@ -1,5 +1,5 @@
 /*
- * The task is to read each row in the input file and check that all numbers are in accending or decending order
+ * The task is to read each row in the input file and check that all numbers are in ascending or descending order
  * Additionally, any two adjacent numbers may not be the same or deffer by more than three
  * Count each row that is valid then report the total
  */
@@ -9,28 +9,28 @@
     public static int Part1(string filePath)
     {
         int validLines = 0;
-        bool isAcending;
-        bool isDecending;
+        bool isAscending;
+        bool isDescending;
         using StreamReader r = new StreamReader(filePath);
         string? line;
 
         while ((line = r.ReadLine()) != null)
         {
             string[] splitLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            isAcending = true;
-            isDecending = true;
+            isAscending = true;
+            isDescending = true;
 
             for (int i = 0; i < splitLine.Length - 1; i++)
             {
                 if (Int32.Parse(splitLine[i]) > Int32.Parse(splitLine[i + 1]))
                 {
-                    isAcending = false;
+                    isAscending = false;
                 }
                 else if (Int32.Parse(splitLine[i]) < Int32.Parse(splitLine[i + 1]))
                 {
-                    isDecending = false;
+                    isDescending = false;
                 }
-                if (!isAcending && !isDecending)
+                if (!isAscending && !isDescending)
                 {
                     // At school I was taught to "never" use goto in c/c++. 
                     // But because I can't break to an outer loop like in Rust, goto seems polite enough for that usecase.
@@ -44,7 +44,7 @@
                     goto skipLine;
                 }
             }
-            if (isAcending || isDecending)
+            if (isAscending || isDescending)
             {
                 // Console.WriteLine($"Valid line found {line}");
                 validLines++;
