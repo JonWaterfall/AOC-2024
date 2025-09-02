c0425c417bab8300fd42bf7c845861eafec4fb24
JonWaterfall <jon.gunnar.fossum@gmail.com>
2025-09-02 14:13:36 +0200

    Fix spelling error and whitespace linting

--- days/day02.cs
+++ days/day02.cs
@@ -1,15 +1,18 @@
 /*
  * The task is to read each row in the input file and check that all numbers are in ascending or descending order
- * Additionally, any two adjacent numbers may not be the same or deffer by more than three
+ * Additionally, any two adjacent numbers may not be the same or differ by more than three
  * Count each row that is valid then report the total
  *
  * Part 2 lets us remove a single number from a line in the input and re-evaluate it. 
  * I have to assume that if the second number is removed that whether a line is ascending or descending might change.
  * ex: 7 9 6 5 4 can be valid by removing the 9, and the line changes from ascending to descending.
+ *
+ * I think the "simplest" would be to just brute force check every removal. Not optimal but makes for easier to read code.
+ * 
  */
 
- static class Day02
- {
+static class Day02
+{
     public static int Part1(string filePath)
     {
         int validLines = 0;
@@ -40,7 +43,7 @@
                     // But because I can't break to an outer loop like in Rust, goto seems polite enough for that usecase.
                     goto skipLine;
                 }
-                // +1 lets me avoid writing a check for zero
+                
                 int num1 = Int32.Parse(splitLine[i]);
                 int num2 = Int32.Parse(splitLine[i + 1]);
                 if (num1 == num2 || Math.Abs(num1 - num2) > 3)
@@ -53,7 +56,7 @@
                 // Console.WriteLine($"Valid line found {line}");
                 validLines++;
             }
-            skipLine:;
+        skipLine:;
         }
 
         return validLines;
